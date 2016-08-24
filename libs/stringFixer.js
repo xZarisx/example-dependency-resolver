dffc15a42db003973f78eeeb0156f776dd4c0f16
Matthew Poulson <matthewpoulson@ldschurch.org>
2016-08-23 19:54:14 -0600

    Add string fixer and fix test to work right

--- libs/stringFixer.js
+++ libs/stringFixer.js
@@ -1,5 +1,7 @@
 // "key: value"
 
 export default (uglyString) => {
-    
+    return uglyString.split(": ").reduce((pre, cur) => {
+        return {[pre]: cur};
+    });
 }
--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,9 @@
 import tape from "tape";
-import stringFixer from "./libs/stringFixer";
+import stringFixer from "../libs/stringFixer";
 
 tape("stringFixer", (test) => {
     const testObject = stringFixer("foo: bar");
 
-    test.equals(testObject, {foo: "bar"});
+    test.deepEqual(testObject, {foo: "bar"});
     test.end();
 });
